8c0161dd8b5517b2f5fbc9ea4b0e8e1eff991698
Lucas Simão <oliveira.lucas20@gmail.com>
2021-12-09 18:03:20 -0400

    Cria a class UseCaseModule

---
+++ app/src/main/java/com/lucassimao/tmdb/UseCaseModule.kt
@@ -0,0 +1,20 @@
+package com.lucassimao.tmdb
+
+import com.lucassimao.tmdb.domain.MovieRepository
+import com.lucassimao.tmdb.domain.usecase.GetMoviesUseCase
+import com.lucassimao.tmdb.domain.usecase.UpdateMoviesUseCase
+import dagger.Module
+import dagger.Provides
+
+@Module
+class UseCaseModule {
+    @Provides
+    fun provideGetMovieUseCase(movieRepository: MovieRepository): GetMoviesUseCase {
+        return GetMoviesUseCase(movieRepository)
+    }
+
+    @Provides
+    fun provideUpdateMovieUseCase(movieRepository: MovieRepository): UpdateMoviesUseCase {
+        return UpdateMoviesUseCase(movieRepository)
+    }
+}
