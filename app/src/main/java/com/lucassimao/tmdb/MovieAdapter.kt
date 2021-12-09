440db5c8f46ea395361a89ff39f37c88370d604a
Lucas Simão <oliveira.lucas20@gmail.com>
2021-12-09 17:05:36 -0400

    Implementa o RecyclerView

--- app/src/main/java/com/lucassimao/tmdb/MovieAdapter.kt
+++ app/src/main/java/com/lucassimao/tmdb/MovieAdapter.kt
@@ -1,9 +1,39 @@
 package com.lucassimao.tmdb
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.lucassimao.tmdb.data.model.Movie
+import com.lucassimao.tmdb.databinding.ListItemBinding
 
-class MovieAdapter {
+class MovieAdapter : RecyclerView.Adapter<MovieViewHolder>() {
+    private val movieList = ArrayList<Movie>()
+
+    fun setList(movies: List<Movie>) {
+        movieList.clear()
+        movieList.addAll(movies)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding: ListItemBinding = DataBindingUtil.inflate(
+            layoutInflater,
+            R.layout.list_item,
+            parent,
+            false
+        )
+
+        return MovieViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        holder.bind(movieList[position])
+    }
+
+    override fun getItemCount() = movieList.size
+
 }
 
 class MovieViewHolder(val binding: ListItemBinding) :
