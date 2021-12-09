64745444f6994c21bcc0e41097a2af114517cbe1
Lucas Simão <oliveira.lucas20@gmail.com>
2021-12-09 16:33:04 -0400

    Cria a função getMoviesFromDB

--- app/src/main/java/com/lucassimao/tmdb/repository/MovieRepositoryImpl.kt
+++ app/src/main/java/com/lucassimao/tmdb/repository/MovieRepositoryImpl.kt
@@ -8,7 +8,7 @@ import com.lucassimao.tmdb.repository.datasource.MovieRemoteDatasource
 
 class MovieRepositoryImpl(
     private val movieRemoteDatasource: MovieRemoteDatasource,
-    private val movieLocalDatasource: MovieDBDatasource,
+    private val movieDBDatasource: MovieDBDatasource,
     private val movieCacheDatasource: MovieCacheDatasource
 ) : MovieRepository{
     override suspend fun getMovies(): List<Movie> {
@@ -29,6 +29,22 @@ class MovieRepositoryImpl(
             }
         } catch (e: Exception) {
 
+        }
+        return movieList
+    }
+
+    private suspend fun getMoviesFromDB(): List<Movie> {
+        lateinit var movieList: List<Movie>
+        try {
+            movieList = movieDBDatasource.getMoviesFromDB()
+        } catch (e: Exception) {
+
+        }
+        if (movieList.isNotEmpty()) {
+            return movieList
+        } else {
+            movieList = getMoviesFromAPI()
+            movieDBDatasource.saveMoviesToDB(movieList)
         }
         return movieList
     }
