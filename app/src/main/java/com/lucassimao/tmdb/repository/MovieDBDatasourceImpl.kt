7414d82fc5de8830e0fb4543722b6a7e5aad4b3a
Lucas Simão <oliveira.lucas20@gmail.com>
2021-12-09 15:57:23 -0400

    Cria a class MovieDBDatasourceImpl

---
+++ app/src/main/java/com/lucassimao/tmdb/repository/MovieDBDatasourceImpl.kt
@@ -0,0 +1,25 @@
+package com.lucassimao.tmdb.repository
+
+import com.lucassimao.tmdb.db.MovieDAO
+import com.lucassimao.tmdb.model.Movie
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class MovieDBDatasourceImpl(private val movieDAO: MovieDAO) : MovieDBDatasource {
+    override suspend fun getMoviesFromDB(): List<Movie> {
+        return movieDAO.getMovies()
+    }
+
+    override suspend fun saveMoviesToDB(movies: List<Movie>) {
+        CoroutineScope(Dispatchers.IO).launch {
+            movieDAO.saveMovies(movies)
+        }
+    }
+
+    override suspend fun clearAll() {
+        CoroutineScope(Dispatchers.IO).launch {
+            movieDAO.deleteAllMovies()
+        }
+    }
+}
