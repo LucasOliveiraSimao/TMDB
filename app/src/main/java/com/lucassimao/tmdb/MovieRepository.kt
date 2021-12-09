807b82596fbb6a7552beaa6fad1be2f77bc57787
Lucas Simão <oliveira.lucas20@gmail.com>
2021-12-09 16:30:20 -0400

    Implementa a interface MovieRepository na class MovieRepositoryImpl

--- app/src/main/java/com/lucassimao/tmdb/MovieRepository.kt
+++ app/src/main/java/com/lucassimao/tmdb/MovieRepository.kt
@@ -1,4 +1,8 @@
 package com.lucassimao.tmdb
 
+import com.lucassimao.tmdb.model.Movie
+
 interface MovieRepository {
+    suspend fun getMovies(): List<Movie>
+    suspend fun updateMovies(): List<Movie>
 }
--- app/src/main/java/com/lucassimao/tmdb/repository/MovieRepositoryImpl.kt
+++ app/src/main/java/com/lucassimao/tmdb/repository/MovieRepositoryImpl.kt
@@ -1,4 +1,22 @@
 package com.lucassimao.tmdb.repository
 
-class MovieRepositoryImpl {
+import com.lucassimao.tmdb.MovieRepository
+import com.lucassimao.tmdb.model.Movie
+import com.lucassimao.tmdb.repository.datasource.MovieCacheDatasource
+import com.lucassimao.tmdb.repository.datasource.MovieDBDatasource
+import com.lucassimao.tmdb.repository.datasource.MovieRemoteDatasource
+
+class MovieRepositoryImpl(
+    private val movieRemoteDatasource: MovieRemoteDatasource,
+    private val movieLocalDatasource: MovieDBDatasource,
+    private val movieCacheDatasource: MovieCacheDatasource
+) : MovieRepository{
+    override suspend fun getMovies(): List<Movie> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun updateMovies(): List<Movie> {
+        TODO("Not yet implemented")
+    }
+    
 }
