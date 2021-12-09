43893707aa35b58c46e1544095eb4b2c0ea474e2
Lucas Simão <oliveira.lucas20@gmail.com>
2021-12-09 19:32:30 -0400

    Adiciona a anotação @Module na class CacheDataModule

--- app/src/main/java/com/lucassimao/tmdb/presentation/di/core/CacheDataModule.kt
+++ app/src/main/java/com/lucassimao/tmdb/presentation/di/core/CacheDataModule.kt
@@ -2,9 +2,11 @@ package com.lucassimao.tmdb.presentation.di.core
 
 import com.lucassimao.tmdb.data.repository.datasource.MovieCacheDatasource
 import com.lucassimao.tmdb.data.repository.datasourceImpl.MovieCacheDatasourceImpl
+import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
 
+@Module
 class CacheDataModule {
     @Singleton
     @Provides
