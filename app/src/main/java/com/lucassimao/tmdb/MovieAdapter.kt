918c2847ad534ca6282a0d2bae2ee465ced11456
Lucas Simão <oliveira.lucas20@gmail.com>
2021-12-09 16:54:13 -0400

    Cria a class MovieViewHolder

--- app/src/main/java/com/lucassimao/tmdb/MovieAdapter.kt
+++ app/src/main/java/com/lucassimao/tmdb/MovieAdapter.kt
@@ -1,4 +1,22 @@
 package com.lucassimao.tmdb
 
+import androidx.recyclerview.widget.RecyclerView
+import com.lucassimao.tmdb.data.model.Movie
+
 class MovieAdapter {
+}
+
+class MovieViewHolder(val binding: ListItemBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(movie: Movie) {
+        binding.titleItemList.text = movie.title
+        binding.descriptionItemList.text = movie.overview
+
+        val posterURL = "https://image.tmdb.org/t/p/w500" + movie.poster_path
+
+        Glide.with(binding.imageItemList.context)
+            .load(posterURL)
+            .into(binding.imageItemList)
+    }
 }
