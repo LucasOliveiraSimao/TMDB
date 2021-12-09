91fafe15b7e1de207c1d8dce72cbca0aaaef6ea9
Lucas Simão <oliveira.lucas20@gmail.com>
2021-12-09 18:01:51 -0400

    Cria a class DatabaseModule

---
+++ app/src/main/java/com/lucassimao/tmdb/DatabaseModule.kt
@@ -0,0 +1,25 @@
+package com.lucassimao.tmdb
+
+import android.content.Context
+import androidx.room.Room
+import com.lucassimao.tmdb.data.db.MovieDAO
+import com.lucassimao.tmdb.data.db.TMDBDatabase
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class DatabaseModule {
+    @Singleton
+    @Provides
+    fun provideDatabase(context: Context): TMDBDatabase {
+        return Room.databaseBuilder(context, TMDBDatabase::class.java, "tmdb_database")
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideMovieDAO(tmdbDatabase: TMDBDatabase): MovieDAO {
+        return tmdbDatabase.movieDao()
+    }
+}
