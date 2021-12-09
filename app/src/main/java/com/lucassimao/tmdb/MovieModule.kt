66f4cd9687313515465c349110cba684fae04f62
Lucas Simão <oliveira.lucas20@gmail.com>
2021-12-09 18:09:58 -0400

    Cria a class MovieModule

---
+++ app/src/main/java/com/lucassimao/tmdb/MovieModule.kt
@@ -0,0 +1,22 @@
+package com.lucassimao.tmdb
+
+import com.lucassimao.tmdb.domain.usecase.GetMoviesUseCase
+import com.lucassimao.tmdb.domain.usecase.UpdateMoviesUseCase
+import com.lucassimao.tmdb.movie.MovieViewModelFactory
+import dagger.Module
+import dagger.Provides
+
+@Module
+class MovieModule {
+    @MovieScope
+    @Provides
+    fun provideMovieViewModelFactory(
+        getMoviesUseCase: GetMoviesUseCase,
+        updateMoviesUseCase: UpdateMoviesUseCase
+    ): MovieViewModelFactory {
+        return MovieViewModelFactory(
+            getMoviesUseCase,
+            updateMoviesUseCase
+        )
+    }
+}
