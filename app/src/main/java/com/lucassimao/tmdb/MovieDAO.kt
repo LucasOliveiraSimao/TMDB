6693759d45fd86feb5aaae95099491d541c0317a
Lucas Simão <oliveira.lucas20@gmail.com>
2021-12-09 15:35:19 -0400

    Cria a interface MovieDAO

---
+++ app/src/main/java/com/lucassimao/tmdb/MovieDAO.kt
@@ -0,0 +1,17 @@
+package com.lucassimao.tmdb
+
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.lucassimao.tmdb.model.Movie
+
+interface MovieDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveMovies(movies: List<Movie>)
+
+    @Query("DELETE FROM popular_movies")
+    suspend fun deleteAllMovies()
+
+    @Query("SELECT * FROM popular_movies")
+    suspend fun getMovies(): List<Movie>
+}
