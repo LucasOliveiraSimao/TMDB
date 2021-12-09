971c5a520f2b920c586c92fc470cdab5c058e63b
Lucas Simão <oliveira.lucas20@gmail.com>
2021-12-09 17:56:33 -0400

    Cria a class CacheDataModule

---
+++ app/src/main/java/com/lucassimao/tmdb/CacheDataModule.kt
@@ -0,0 +1,14 @@
+package com.lucassimao.tmdb
+
+import com.lucassimao.tmdb.data.repository.datasource.MovieCacheDatasource
+import com.lucassimao.tmdb.data.repository.datasourceImpl.MovieCacheDatasourceImpl
+import dagger.Provides
+import javax.inject.Singleton
+
+class CacheDataModule {
+    @Singleton
+    @Provides
+    fun provideMovieCacheDatasource(): MovieCacheDatasource {
+        return MovieCacheDatasourceImpl()
+    }
+}
