4208332b240b6f03006e567b716f172b0e81b15d
Lucas Simão <oliveira.lucas20@gmail.com>
2021-12-09 19:31:35 -0400

    Adiciona a anotação @Module na class DBDataModule

--- app/src/main/java/com/lucassimao/tmdb/presentation/di/core/DBDataModule.kt
+++ app/src/main/java/com/lucassimao/tmdb/presentation/di/core/DBDataModule.kt
@@ -3,9 +3,11 @@ package com.lucassimao.tmdb.presentation.di.core
 import com.lucassimao.tmdb.data.db.MovieDAO
 import com.lucassimao.tmdb.data.repository.datasource.MovieDBDatasource
 import com.lucassimao.tmdb.data.repository.datasourceImpl.MovieDBDatasourceImpl
+import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
 
+@Module
 class DBDataModule {
     @Singleton
     @Provides
