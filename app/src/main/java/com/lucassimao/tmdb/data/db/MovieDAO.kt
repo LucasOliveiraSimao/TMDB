eba3af26ade8b2383db13b6f4481d2e412c7a48b
Lucas Simão <oliveira.lucas20@gmail.com>
2021-12-09 19:23:27 -0400

    Adiciona a anotação @Dao na interface MovieDAO

--- app/src/main/java/com/lucassimao/tmdb/data/db/MovieDAO.kt
+++ app/src/main/java/com/lucassimao/tmdb/data/db/MovieDAO.kt
@@ -1,10 +1,12 @@
 package com.lucassimao.tmdb.data.db
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.lucassimao.tmdb.data.model.Movie
 
+@Dao
 interface MovieDAO {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun saveMovies(movies: List<Movie>)
