6372bf02ef8f814ebff2518dbb4934b6a81a7120
Lucas Simão <oliveira.lucas20@gmail.com>
2021-12-09 15:33:37 -0400

    Cria a abstract class TMDBDatabase

---
+++ app/src/main/java/com/lucassimao/tmdb/TMDBDatabase.kt
@@ -0,0 +1,14 @@
+package com.lucassimao.tmdb
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.lucassimao.tmdb.model.Movie
+
+@Database(
+    entities = [Movie::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class TMDBDatabase:RoomDatabase() {
+
+}
